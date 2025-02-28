9e6ace53a96905fef9a92c3d59543c8b7dc60b74
chhayashah22 <shahchhaya607@gmail.com>
2025-02-28 09:09:30 +0530

    Force HTTPS in Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  
+  server: {
+    https: true,
+    host: "0.0.0.0",
+    port: process.env.PORT || 10000},
   plugins: [react()],
 })
